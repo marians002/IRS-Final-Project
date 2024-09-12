f818e0aa30a59cc654ee9da4e168dfafa064982c
Marian <mariansusanaas@gmail.com>
2024-09-11 20:07:58 -0400

    fix bugs

--- src/map_names.py
+++ src/map_names.py
@@ -5,7 +5,7 @@ def map_names_to_ids(file_path='sample_names.txt'):
     # Remove any leading/trailing whitespace characters (like newlines)
     names = [name.strip() for name in names]
 
-    # Create a dictionary mapping names to IDs
+    # Create a dictionary mapping names to IDs (Angelica Powers: 1)
     name_to_id = {name: i+1 for i, name in enumerate(names)}
 
     return name_to_id
